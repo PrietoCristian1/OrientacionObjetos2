fe9088852de6f50073e2a4c368d74b2b837d0d6f
PrietoCristian1 <cp3479909@gmail.com>
2024-03-12 10:26:56 -0300

    Creacion de la clase Main y Tiempo. La repuesta de porque la clase Tiempo es anemico esta comentada en la misma clase.

--- src/java/tp0/Tiempo.java
+++ src/java/tp0/Tiempo.java
@@ -7,11 +7,7 @@ public class Tiempo {
      * Tiempo Anemico, su funcion principal es solo proporcionar acceso a la fecha actual y no contiene comportamiento propio del objeto.
      * En este caso se utiliza el objeto como si fuera una estructura de datos donde accedo unicamente a recuaperar/guardar informacion.
      */
-    private LocalDate fechaActual;
-
-    public Tiempo() {
-        this.fechaActual = LocalDate.now();
-    }
+    private final LocalDate fechaActual = LocalDate.now();
 
     public LocalDate getFechaActual() {
         return this.fechaActual;
